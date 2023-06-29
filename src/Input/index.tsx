3e8c5c3e3ac34f9232075990ee74f55549f51dc8
iofjuupasli <iofjuupasli@gmail.com>
2023-06-29 11:59:31 +0200

    Improve InputDescription design

--- src/Input/index.tsx
+++ src/Input/index.tsx
@@ -327,7 +327,7 @@ InputMessage.displayName = "InputMessage";
 export const InputHeader = div`text-xl mb-3`;
 InputHeader.displayName = "InputHeader";
 
-export const InputDescription = div`text-gray-700 mb-3`;
+export const InputDescription = div`text-gray-600 mb-3 mt-2 text-sm`;
 InputDescription.displayName = "InputDescription";
 
 export const InputContainer = forwardRef<
